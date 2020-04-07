f5a70b130c49064dc07241658cee4f87b709cb07
Kindrat22 <43355118+Kindrat22@users.noreply.github.com>
2020-04-07 13:53:03 +0300

    [VG-6]-Integrate_Localization (#13)
    [VG-6]-Integrate_Localization

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import i18n from './i18n';
 
-function App() {
+
+import { withNamespaces } from 'react-i18next';
+//'t' funct is to change languages
+function App({ t }) {
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+      {/*This two buttons change language*/}
+      <button onClick={() => changeLanguage('ua')}>ua</button>
+      <button onClick={() => changeLanguage('en')}>en</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        { /* there you can see 't' func to load locales from 'locales' folder
+        you also can write your own localization at file 'translation.json'
+        at en or ua folders -->*/}
+          {t("welcomeMessage")}
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t("learnReactButton")}
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withNamespaces()(App);
