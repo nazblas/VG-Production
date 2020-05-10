cb76ac8d853c5b555b60945faa00985e23df4d7a
Nefito <vkorzhov00@gmail.com>
2020-05-10 19:56:53 +0300

    add dish component

---
+++ front_end/src/containers/Dish_info/Dish_info.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import RecommendedDish from '../../components/recommendedDish.jsx'
+import CategoryBox from '../../components/categoryBox.jsx'
+import Searchbar from '../Searchbar/Searchbar.jsx'
+import { withNamespaces } from 'react-i18next'
+
+class Dish_info extends React.Component{
+    render(){
+
+        const {t} = this.props;
+
+        return(
+            <div className="card mb-3 recipeCard">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="..." className="card-img" alt={t("imgAlt")}>
+                            <svg class="bi bi-box-arrow-in-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M15 8a.5.5 0 00-.5-.5h-9a.5.5 0 000 1h9A.5.5 0 0015 8z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 011 13V3a1.5 1.5 0 011.5-1.5h8A1.5 1.5 0 0112 3v1.5a.5.5 0 01-1 0V3a.5.5 0 00-.5-.5h-8A.5.5 0 002 3v10a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-1.5a.5.5 0 011 0V13a1.5 1.5 0 01-1.5 1.5h-8z" clip-rule="evenodd"/>
+                            </svg>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{t("dishTitle")}</h5>
+                            <p className="card-text">{t("dishDesc")}</p>
+                        </div>
+                    </div>
+                    <div className="col-md-12">
+                        <p>{t("dishRecipe")}</p>
+                    </div>
+                    <div className="col-md-12">
+                        <button type="button" class="btn btn-warning btn-lg btn-block">Back</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default withNamespaces()(Dish_info)
